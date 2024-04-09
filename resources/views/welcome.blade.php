6fb0ac1ded04a45d6b5b460906401e133dd3f85b
Jacka10086 <496806255@qq.com>
2024-04-09 01:28:00 +0100

    2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
-                                               Jacka10086
+                                               Jacka100666
                 
                                             </p>
                                         </div>
